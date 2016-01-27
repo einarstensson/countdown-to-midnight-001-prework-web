f965e94dbd5c4231257f418a1a1c90355e6485c2
einarstensson <einar.stensson@gmail.com>
2016-01-27 15:10:37 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    #Although a second has not yet passed, this is how people do it on New Year's, people 
+    #chant first then count so the sleep(1) method comes after puts.
+    puts "#{seconds} SECOND(S)!" 
+    sleep(1)      
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
